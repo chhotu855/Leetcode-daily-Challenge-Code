c0d07f5845f9abcfaec72f4aa8c5ec96cc72a685
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-05-02 13:39:02 +0530

    Update may2_2441.cpp

--- May24/may2_2441.cpp
+++ May24/may2_2441.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+     int n = nums.size();
+     sort(nums.begin(),nums.end());
+     int left = 0 , right = n-1;
+     while(left < right){
+        if(nums[left]+nums[right] == 0) return nums[right];
+        else if(nums[left]+nums[right] < 0) left++;
+        else right--;
+     }
+     return -1;
+    }
+};
 
+/* method 2 *\
+  class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+     int n = nums.size();
+     unordered_set<int>st;
+     int ans = -1;
+     for(int i=0; i<n; i++){
+        if(st.find(nums[i]*-1) != st.end()){
+            ans = max(ans,abs(nums[i]));
+        }
+         st.insert(nums[i]);
+     }
+     return ans;
+    }
+};
