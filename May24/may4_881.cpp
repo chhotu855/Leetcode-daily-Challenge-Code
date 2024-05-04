31a0cba6e23662330eec0acc58a116e9644ba205
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-05-04 12:11:38 +0530

    Update may4_881.cpp

--- May24/may4_881.cpp
+++ May24/may4_881.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+     int n = people.size();
+     sort(people.begin(),people.end());
+      int left = 0 , right = n-1;
+      int boat = 0;
+      while(left <= right){
+        if(people[left]+people[right] <= limit){
+          right--;
+          left++;
+          boat++;
+        }
+        else if(people[left]+people[right] > limit){
+            right--;
+            boat++;
+        }
+      } 
+      return boat; 
+    }
+};
