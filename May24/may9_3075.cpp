44ca3b2c0e5c5287573a0adc86f0f52f0d77a68e
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-05-09 17:02:19 +0530

    Update may9_3075.cpp

--- May24/may9_3075.cpp
+++ May24/may9_3075.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+      int n = happiness.size();
+      sort(happiness.begin(),happiness.end(),greater<int>());
+      long long ans = 0;
+      int loopcnt = 0;
+      for(int i=0; i<k; i++){
+        if(happiness[i]-loopcnt > 0)
+        ans += (happiness[i]-loopcnt);
+        loopcnt++;
+      }
+      return ans;  
+    }
+};
+method 2(using maxheap)
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        int n = happiness.size();
+      priority_queue<int>maxh;
+      for(int i=0; i<n; i++){
+        maxh.push(happiness[i]);
+      }
+      long long ans = 0;
+      long long  loopcnt = 0;
+      while(k > 0){
+        long long loopans = maxh.top()-loopcnt;
+        maxh.pop();
+        ans += (loopans > 0) ? loopans : 0;
+        loopcnt++;
+        k--;
+      }
+      return ans;
+    }
+};
