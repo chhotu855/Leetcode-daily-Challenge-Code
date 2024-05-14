ef5c7963410d4323a4c022b31eb65c1a13aa8153
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-05-14 18:49:16 +0530

    Update may11_857.cpp

--- May24/may11_857.cpp
+++ May24/may11_857.cpp
@@ -1,1 +1,24 @@
-
+class Solution {
+public:
+    double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+       int n = quality.size();
+       vector<pair<double,int>>workers;
+       for(int i=0; i<n; i++){
+        workers.push_back({(wage[i]*1.0)/quality[i] , quality[i]});
+       } 
+       sort(workers.begin(),workers.end());
+       priority_queue<int>maxh;
+       double ans = DBL_MAX , qualitySum = 0;
+       for(auto it : workers){
+        double ratio = it.first;
+        qualitySum += it.second;
+        maxh.push(it.second);
+        if(maxh.size() > k){
+            qualitySum -= maxh.top();
+            maxh.pop();
+        }
+        if(maxh.size() == k) ans = min(ans,ratio*qualitySum);
+       }
+       return ans;
+    }
+};
