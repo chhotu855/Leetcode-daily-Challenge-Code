a4783b9db5893d8c8d76319cdc665edf2851e636
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-05-20 23:46:42 +0530

    Update may20_1863.cpp

--- May24/may20_1863.cpp
+++ May24/may20_1863.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+   int helper(vector<int>& nums ,int index ,int currentXor){
+    if(index == nums.size()) return currentXor;
+    int notTakenXor = helper(nums , index+1 , currentXor);
+    int takenXor = helper(nums , index+1 , currentXor^nums[index]);
+    return notTakenXor+takenXor;
+   }
+    int subsetXORSum(vector<int>& nums) {
+       return helper(nums , 0 ,0);
+    }
+};
