4d49a7b3fbdd4bdb7dd0c1a1d049fb619e1a36d1
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-05-21 20:10:34 +0530

    Update may21_78.cpp

--- May24/may21_78.cpp
+++ May24/may21_78.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+   void helper(vector<int>& nums ,int index ,vector<vector<int>>& ans , vector<int>& temp){
+    if(index == nums.size()){
+        ans.push_back(temp);
+        return;
+    }
+    temp.push_back(nums[index]);
+    helper(nums ,index+1 , ans ,temp);
+    temp.pop_back();
+    helper(nums , index+1 , ans ,temp);
+   }
+    vector<vector<int>> subsets(vector<int>& nums) {
+      vector<vector<int>>ans;
+      vector<int>temp;
+      helper(nums , 0 ,ans , temp);
+      return ans;  
+    }
+};
