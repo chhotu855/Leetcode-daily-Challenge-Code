ffac4861e627a2c71f9ff877f08e3a3169fca92f
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-05-22 23:27:02 +0530

    Update may22_131.cpp

--- May24/may22_131.cpp
+++ May24/may22_131.cpp
@@ -1,1 +1,29 @@
-
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+     int n = s.size();
+     vector<string>path;
+     vector<vector<string>>ans;
+     helper(0,s,path,ans);
+     return ans;
+    }
+    void helper(int index , string s , vector<string>& path , vector<vector<string>>& ans){
+        if(index == s.size()){
+            ans.push_back(path);
+            return;
+        }
+        for(int i=index; i<s.size(); i++){
+            if(palindrome(s,index,i)){
+                path.push_back(s.substr(index ,i-index+1));
+            helper(i+1,s,path,ans);
+            path.pop_back();
+            } 
+        }
+    }
+    bool palindrome(string s , int left , int right){
+        while(left <= right){
+            if(s[left++] != s[right--]) return false;
+        }
+        return true;
+    }
+};
