7782b6c4537b37d4b57cc4172c0ffa674c37614d
chhotu855 <127241499+chhotu855@users.noreply.github.com>
2024-06-02 10:13:42 +0530

    Update june2_344.cpp

--- June24/june2_344.cpp
+++ June24/june2_344.cpp
@@ -1,1 +1,10 @@
-
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+       int n = s.size();
+       int left = 0 , right = n-1;
+       while(left <= right){
+        swap(s[left++],s[right--]);
+       } 
+    }
+};
